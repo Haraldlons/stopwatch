2a21cdd5c527c7bb488998eb4d3eb2b8f31760c0
Harald Lønsethagen <haraldlons@gmail.com>
2023-01-05 20:57:37 +0100

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import marky = require('marky');
 const start = marky.mark;
 
 const stop = (name: string) => {
-    const result = marky.stop(name);
-    return result.duration;
+  const result = marky.stop(name);
+  return result.duration;
 };
 
 const stopPrint = (name: string) => {
-    const duration = stop(name);
-    console.log(`${name} took ${duration}ms`);
-    return duration;
+  const duration = stop(name);
+  console.log(`${name} took ${duration}ms`);
+  return duration;
 };
 
 export { start, stop, stopPrint };
